80ed7ac96dd80178dfaca3d4bd566781a943b115
dannyradden <dradden@gmail.com>
2017-11-02 16:23:31 -0600

    forgot the rest of files

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,15 +1,18 @@
 const passport = require('passport');
+const checkPath = require('../middlewares/checkPath');
 
 module.exports = app => {
   app.get(
     '/auth/google',
+    checkPath,
     passport.authenticate('google', {
       scope: ['profile', 'email']
     })
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-    res.redirect('/surveys');
+    res.redirect(req.session.returnTo);
+    delete req.session.returnTo;
   });
 
   app.get('/api/current_user', (req, res) => {
